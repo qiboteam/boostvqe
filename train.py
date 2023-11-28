ba707b56c8e1e581cf6b0308e542ae70ec6ac2a9
MatteoRobbiati <robbiati.matteo@gmail.com>
2023-11-28 16:33:50 +0100

    parsing hyper parameters

--- train.py
+++ train.py
@@ -1,26 +1,34 @@
+import argparse
+
 import numpy as np
 
 from qibo.models.variational import VQE
 from qibo import hamiltonians
+from ansatze import build_circuit
 
-from ansatze import build_floquet
-
-nqubits = 4
-nlayers = 5
-
-h = hamiltonians.XXZ(nqubits=nqubits)
-c = build_circuit(nqubits=nqubits, nlayers=nlayers)
-print(c.draw())
-
-nparams = len(c.get_parameters())
-
-# initialize VQE
-vqe = VQE(circuit=c, hamiltonian=h)
-initial_parameters = np.random.randn(nparams)
+parser = argparse.ArgumentParser(description='boostvqe hyper-parameters.')
+parser.add_argument("--nqubits", default=6, type=int)
+parser.add_argument("--nlayers", default=5, type=int)
 
 
-result = vqe.minimize(initial_parameters)
+def main(args):
+    h = hamiltonians.XXZ(nqubits=args.nqubits)
+    c = build_circuit(nqubits=args.nqubits, nlayers=args.nlayers)
 
-print(result)
+    # just print the circuit
+    print(c.draw())
+
+    nparams = len(c.get_parameters())
+
+    # initialize VQE
+    vqe = VQE(circuit=c, hamiltonian=h)
+    initial_parameters = np.random.randn(nparams)
 
 
+    result = vqe.minimize(initial_parameters)
+
+    print(result)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main(args)
