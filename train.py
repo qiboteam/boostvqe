a4049b936bd82d688eee35d2fea2de962bbf9aab
MatteoRobbiati <robbiati.matteo@gmail.com>
2023-11-29 17:31:28 +0100

    parsing hyper-parameters

--- train.py
+++ train.py
@@ -12,21 +12,21 @@ parser.add_argument("--nlayers", default=5, type=int)
 
 
 def main(args):
+    """VQE training and DBI boosting."""
+    # build hamiltonian and variational quantum circuit
     h = hamiltonians.XXZ(nqubits=args.nqubits)
     c = build_circuit(nqubits=args.nqubits, nlayers=args.nlayers)
 
     # just print the circuit
     print(c.draw())
-
     nparams = len(c.get_parameters())
 
     # initialize VQE
     vqe = VQE(circuit=c, hamiltonian=h)
     initial_parameters = np.random.randn(nparams)
 
-
     result = vqe.minimize(initial_parameters)
-
+    # visualize the results
     print(result)
 
 if __name__ == "__main__":
