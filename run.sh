a8a5e145bc5940f5c60421c62c1b0d2537d1857c
MatteoRobbiati <robbiati.matteo@gmail.com>
2023-12-08 11:17:03 +0100

    updated results folders

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ n_start=2
 n_end=6
 
 for ((n=$n_start; n<=$n_end; n++)); do
-    python train.py --nqubits 3 --nlayers $n --optimizer BFGS --nthreads 16 --output_folder ./results/test_folder_$n
+    python train.py --nqubits 3 --nlayers $n --optimizer BFGS --nthreads 16 
 done
