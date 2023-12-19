e98d23a241d9d75c1f0f23624c86029167d1c834
Edoardo-Pedicillo <edoardo.pedicillo@gmail.com>
2023-12-19 18:26:11 +0400

    update run_sequential

--- run_sequential.sh
+++ run_sequential.sh
@@ -4,5 +4,5 @@ n_start=2
 n_end=6
 
 for ((n=$n_start; n<=$n_end; n++)); do
-    python train.py --nqubits 3 --nlayers $n --optimizer BFGS --nthreads 16
+    python train_vqe.py --nqubits 3 --nlayers $n --optimizer BFGS --nthreads 16
 done
