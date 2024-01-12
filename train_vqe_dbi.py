c6fcfbdcb2f087d025058284492449961e12e828
MatteoRobbiati <robbiati.matteo@gmail.com>
2024-01-12 14:31:13 +0100

    add new arguments, parse hamiltonian matrix, construct backend

--- train_vqe_dbi.py
+++ train_vqe_dbi.py
@@ -5,8 +5,8 @@ from pathlib import Path
 
 import numpy as np
 import qibo
+from qibo.backends import construct_backend
 from qibo import hamiltonians
-from qibo.backends.numpy import NumpyBackend
 from qibo.models.dbi.double_bracket import (
     DoubleBracketGeneratorType,
     DoubleBracketIteration,
@@ -14,12 +14,10 @@ from qibo.models.dbi.double_bracket import (
 
 from ansatze import build_circuit
 from plotscripts import plot_matrix, plot_results
-from utils import OPTIMIZATION_FILE, PARAMS_FILE, dump_json, json_load
+from utils import OPTIMIZATION_FILE, PARAMS_FILE, HAMILTONIAN_FILE, dump_json, json_load
 
 logging.basicConfig(level=logging.INFO)
 qibo.set_backend("numpy")
-NSTEPS = 1
-STEP = 1e-1
 DBI_FILE = "dbi_matrix"
 DBI_REULTS = "dbi_output.json"
 
@@ -28,12 +26,20 @@ def main(args):
     """
     Load the VQE training and then apply `NSTEPS` of DBI.
     """
+    #  load numpy objects
     data = json_load(f"{args.folder}/{OPTIMIZATION_FILE}")
+    ham_matrix = np.load(f"{args.folder}/{HAMILTONIAN_FILE}")
+    circ_params = np.load(f"{args.folder}/{PARAMS_FILE}")[args.starting_from_epoch]
 
-    ham = hamiltonians.XXZ(nqubits=data["nqubits"])  # TODO: move out
+    # construct backend 
+    backend = construct_backend(backend=data["backend"], platform=data["platform"])
+
+    # loading VQE circuit and hamiltonian
+    ham = hamiltonians.Hamiltonian(nqubits=data["nqubits"], matrix=ham_matrix)
     circ = build_circuit(nqubits=data["nqubits"], nlayers=data["nlayers"])
-    circ_params = np.load(f"{args.folder}/{PARAMS_FILE}")[-1]
     circ.set_parameters(circ_params)
+
+    # new hamiltonian shape after applying VQE unitary
     matrix_circ = np.matrix(circ.unitary())
     new_ham = matrix_circ.getH() @ ham.matrix @ matrix_circ
 
@@ -42,12 +48,13 @@ def main(args):
         hamiltonian=qibo.hamiltonians.Hamiltonian(data["nqubits"], matrix=new_ham),
         mode=DoubleBracketGeneratorType.group_commutator,
     )
+
     if args.step_opt:
         step = dbi.hyperopt_step(
             step_min=1e-4, step_max=1, max_evals=1000.0, verbose=True
         )
     else:
-        step = STEP
+        step = args.stepsize
 
     plot_matrix(dbi.h.matrix, path=args.folder, title="Before")
 
@@ -58,7 +65,7 @@ def main(args):
         dbi(step=step, d=dbi.diagonal_h_matrix)
         hist.append(dbi.off_diagonal_norm)
 
-    zero_state = NumpyBackend().zero_state(data["nqubits"])
+    zero_state = backend.zero_state(data["nqubits"])
     ene_fluct_dbi = dbi.energy_fluctuation(zero_state)
     energy = dbi.h.expectation(zero_state)
     logging.info(f"Energy: {energy}")
@@ -80,6 +87,39 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="VQE with DBI training hyper-parameters."
     )
-    parser.add_argument("--folder", type=str)
-    parser.add_argument("--step_opt", default=False, type=bool)
+    parser.add_argument(
+        "--folder", 
+        type=str, 
+        help="Path to the folder in which training data are saved"
+        )
+    parser.add_argument(
+        "--starting_from_epoch", 
+        type=int, 
+        default=-1, 
+        help="From which training epoch loading the parameters."
+        )
+    parser.add_argument(
+        "--boost_steps", 
+        type=int, 
+        default=None, 
+        help="Number of times the DBI procedure is going to be used."
+        )
+    parser.add_argument(
+        "--dbi_steps", 
+        type=int, 
+        default=1,
+        help="Number of DBI iterations every time the DBI is called."
+        )
+    parser.add_argument(
+        "--stepsize", 
+        type=float, 
+        default=0.01,
+        help="DBI step size."
+        )
+    parser.add_argument(
+        "--step_opt", 
+        type=bool, 
+        default=False,
+        help="Set to True to hyperoptimize the DBI step size."
+        )
     main(parser.parse_args())
