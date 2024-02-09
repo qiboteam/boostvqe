fixing gradients plot

plotscripts.py
@@ -1,10 +1,7 @@
 import json
-import os.path
-import pathlib
 
 import matplotlib.pyplot as plt
 import numpy as np
-from qibo.backends import GlobalBackend
 
 from utils import (
     DBI_ENERGIES,
@@ -93,7 +90,7 @@ def plot_loss(
                 len(dbi_energies[str(i)]) + len(loss_vqe[str(i)]) + start - 1,
             ),
             dbi_energies[str(i)],
-            color=GREEN,
+            color=RED,
             lw=1.5,
             label="DBI",
         )
@@ -111,7 +108,7 @@ def plot_loss(
             ),
             dbi_energies[str(i)] - dbi_fluctuations[str(i)],
             dbi_energies[str(i)] + dbi_fluctuations[str(i)],
-            color=GREEN,
+            color=RED,
             alpha=0.4,
         )
 
@@ -121,7 +118,15 @@ def plot_loss(
         - 2 * config["nboost"]
         + 1
     )
-    plt.hlines(target_energy, 1, max_length, color="red", lw=1, label="Target energy")
+    plt.hlines(
+        target_energy,
+        1,
+        max_length,
+        color="black",
+        lw=1,
+        label="Target energy",
+        ls="--",
+    )
     plt.xlabel("Iterations")
     plt.ylabel("Loss")
     handles, labels = plt.gca().get_legend_handles_labels()
@@ -161,13 +166,24 @@ def plot_gradients(
         label=r"$\langle |\partial_{\theta_i}\text{L}| \rangle_i$",
     )
     for b in range(config["nboost"] - 1):
-        plt.vlines(
-            (b + 1) * config["boost_frequency"],
-            0,
-            np.max(ave_grads),
-            color="black",
-            lw=1,
-        )
+        boost_x = config["boost_frequency"] * (b + 1)
+        if b == 0:
+            plt.plot(
+                (boost_x, boost_x + 1),
+                (ave_grads[boost_x - 1], ave_grads[boost_x]),
+                color=RED,
+                lw=1.5,
+                alpha=1,
+                label="Step after DBI",
+            )
+        else:
+            plt.plot(
+                (boost_x, boost_x + 1),
+                (ave_grads[boost_x - 1], ave_grads[boost_x]),
+                color=RED,
+                lw=1.6,
+                alpha=1,
+            )
     plt.yscale("log")
     plt.xlabel("Iterations")
     plt.ylabel("Gradients magnitude")