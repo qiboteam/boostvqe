c05e39bc6e2100b87a6a19c5548b8578c48e3645
Andrea <andreapasquale97@gmail.com>
2024-02-12 11:56:18 +0400

    refactor: Increase frequency

--- vqe.sh
+++ vqe.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python main.py --nqubits 5 --nlayers 1 --optimizer Powell --output_folder test --backend numpy --nboost 0 --dbi_steps 0
+python main.py --nqubits 5 --nlayers 1 --optimizer Powell --output_folder test --backend numpy --nboost 0 --dbi_steps 0 --boost_frequency 1000
