3098b4fc25baced8376e869d257fbec13f8df4fc
MatteoRobbiati <robbiati.matteo@gmail.com>
2024-05-28 12:37:24 +0400

    working on the nruns plot

--- run.sh
+++ run.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
-#SBATCH --job-name=boostvqe
-#SBATCH --output=boostvqe.log
+#SBATCH --job-name=adamch
+#SBATCH --output=bp_regime.log
 
-NQUBITS=2
+NQUBITS=10
+NLAYERS=10
 
-NLAYERS=1
-DBI_STEPS=0
-NBOOST=0
-NSHOTS=100000
+DBI_STEPS=2
+NBOOST=2
+BOOST_FREQUENCY=100
+
+NSHOTS=1000
+SEED=42
+
 OPTIMIZER="sgd"
 BACKEND="tensorflow"
-TOL=1e-8
-BOOST_FREQUENCY=500
+OPTIMIZER_OPTIONS="{ \"optimizer\": \"Adam\", \"learning_rate\": 0.01, \"nmessage\": 1, \"nepochs\": $BOOST_FREQUENCY }"
 
 python main.py  --nqubits $NQUBITS --nlayers $NLAYERS --optimizer $OPTIMIZER \
-                --output_folder results/debugging --backend $BACKEND --tol $TOL \
-                --dbi_step $DBI_STEPS --seed 42 \
-                --boost_frequency $BOOST_FREQUENCY --nboost $NBOOST
+                --output_folder results/debugging --backend $BACKEND \
+                --dbi_step $DBI_STEPS --seed $SEED \
+                --boost_frequency $BOOST_FREQUENCY --nboost $NBOOST \
+                --optimizer_options "$OPTIMIZER_OPTIONS"
