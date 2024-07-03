a98c9a255f0e5d5575d8f5955378d44203b78ce2
Edoardo-Pedicillo <edoardo.pedicillo@gmail.com>
2024-07-03 13:22:29 +0400

    add plots

--- extras/plot_boosts.py
+++ extras/plot_boosts.py
@@ -92,70 +92,73 @@ def plot_jumps(path, nqubits, nlayers, seed, rotation_type, optimizer, epochs, t
         energies.append(
             load_jumps_given_boosting(f"{vqepath}/{rotation_type}_{optimizer}_{e}e_3s")
         )
+    energies = np.array(energies)
 
     nlosses = len(losses)
     delta = nlosses / 50
 
-    plt.figure(figsize=(6, 6 * 6 / 8))
-    plt.plot(losses, color="royalblue", lw=1, alpha=1, label="VQE loss")
-    plt.plot(
+    _, (a0, a1) = plt.subplots(2, 1, sharex=True, gridspec_kw={"height_ratios": [6, 2]})
+    a0.plot(losses, color="royalblue", lw=1, alpha=1, label="VQE loss")
+    a0.plot(
         losses_1Lmore,
         color="royalblue",
         lw=1,
-        alpha=0.8,
+        alpha=0.7,
         label="VQE loss +1 layer",
-        ls="--",
     )
-    plt.plot(
+    a0.plot(
         losses_2Lmore,
         color="royalblue",
         lw=1,
-        alpha=0.6,
+        alpha=0.5,
         label="VQE loss +2 layer",
-        ls="-.",
     )
-
-    plt.hlines(
+    a0.hlines(
         true_ground_energy,
         0,
         len(losses),
         lw=1,
         color="black",
-        ls="-",
+        ls="--",
+        alpha=0.4,
         label="Target energy",
     )
-    for j, e in enumerate(epochs):
-        for i, energy in enumerate(reversed(energies[j])):
-            plt.vlines(
-                epochs[j],
-                energy,
-                losses[epochs[j]],
-                color=steps_colors[i],
-                ls="-",
-                lw=1,
-            )
-            if j == 0:
-                plt.hlines(
-                    energy,
-                    epochs[j] - delta,
-                    epochs[j] + delta,
-                    color=steps_colors[i],
-                    lw=1,
-                    label=f"{len(energies[j])-i} GCI steps",
-                )
-            else:
-                plt.hlines(
-                    energy,
-                    epochs[j] - delta,
-                    epochs[j] + delta,
-                    color=steps_colors[i],
-                    lw=1,
-                )
-    plt.ylim(-15.4, -13)
-    plt.legend(ncols=2)
-    plt.xlabel("Epoch")
-    plt.ylabel("Energy")
-    plt.title(f"{nqubits} qubits, {nlayers} layers, XXZ")
+
+    for i in range(energies.shape[1]):
+        energy = energies[:, i]
+        a0.vlines(epochs, energy, losses[epochs], color=steps_colors[i], ls="-", lw=1)
+        a0.hlines(
+            energy,
+            epochs - delta,
+            epochs + delta,
+            color=steps_colors[i],
+            lw=1,
+            label=f"{i+1} GCI steps",
+        )
+        a1.plot(
+            epochs,
+            1 - (energies[:, i] / true_ground_energy),
+            color=steps_colors[i],
+            lw=1,
+        )
+    a1.plot(
+        1 - (np.array(losses) / true_ground_energy),
+        color="royalblue",
+        lw=1,
+        alpha=1,
+        label="VQE loss",
+    )
+
+    a1.set_ylim(2e-5, 1e-1)
+    a0.set_ylim(-15.4, -13)
+    a0.legend(ncols=2)
+    a1.set_xlabel("Epoch")
+    a1.set_yscale("log")
+    a1.set_ylabel(r"$ 1-\frac{E}{E_0} $")
+    a1.set_yticks([1e-2, 1e-3, 1e-4])
+    a0.set_ylabel("Energy")
+    a0.set_title(f"{nqubits} qubits, {nlayers} layers, XXZ")
+    plt.subplots_adjust(wspace=0, hspace=0)
     plt.savefig(f"{title}.png", dpi=600)
 
 
@@ -165,22 +168,13 @@ def scatterplot_acc_vs_gates(
     vqepath = f"{path}sgd_{nqubits}q_{nlayers}l_{seed}"
     # load losses
     losses = dict(np.load(f"{path}sgd_{nqubits}q_{nlayers}l_{seed}/energies.npz"))["0"]
-    losses_1Lmore = dict(
-        np.load(f"{path}sgd_{nqubits}q_{nlayers+1}l_{seed}/energies.npz")
-    )["0"]
-    losses_2Lmore = dict(
-        np.load(f"{path}sgd_{nqubits}q_{nlayers+2}l_{seed}/energies.npz")
-    )["0"]
 
     # load boosts executed at given epochs
     cgates, accuracies, vqe_cg, vqe_cg_1Lmore, vqe_cg_2Lmore = [], [], [], [], []
 
     colors = ["royalblue"]
     colors.extend(list(sns.color_palette("Reds", n_colors=3).as_hex()))
-
-    labels = ["VQE training", "1 GCI step", "2 GCI steps", "3 GCI steps"]
-    size = 6
-    alpha = 1
+    markers = ["o", "s", "s", "s"]
 
     for j, e in enumerate(epochs):
         # for each epoch load 2q gates, accuracies and gci nfval for each of 3 steps
@@ -207,10 +201,10 @@ def scatterplot_acc_vs_gates(
         cgates.append(gci_cg)
         accuracies.append(acc)
 
-    vqe_acc = abs(np.array(losses[epochs]) - true_ground_energy)
+    vqe_acc = abs(1 - np.array(losses[epochs]) / true_ground_energy)
+    accuracies = 1 - (np.array(accuracies) + true_ground_energy) / true_ground_energy
     print(vqe_acc)
 
-    min_cg = min(gci_cg)
     cgates = np.hstack((np.array(vqe_cg).reshape(-1, 1), np.array(cgates)))
     accuracies = np.hstack((np.array(vqe_acc).reshape(-1, 1), np.array(accuracies)))
 
@@ -220,16 +214,15 @@ def scatterplot_acc_vs_gates(
 
     # looping over algorithms (dim = 4)
     for j in range(np.array(cgates).shape[1]):
-        plt.plot(
-            np.array(cgates).T[j],
-            np.array(accuracies).T[j],
-            color=colors[j],
-            lw=1,
-            alpha=0.5,
-            label=labels[j],
-        )
-        # looping over epochs
         for i in range(np.array(cgates).shape[0]):
+            if j == 0:
+                if i != np.array(cgates).shape[0] - 1:
+                    plt.annotate(
+                        "",
+                        xy=(cgates[i][j], accuracies[i][j]),
+                        xytext=(cgates[i + 1][j], accuracies[i + 1][j]),
+                        arrowprops=dict(arrowstyle="<-", color=colors[0]),
+                    )
             if j != np.array(cgates).shape[1] - 1:
                 plt.annotate(
                     "",
@@ -241,72 +234,24 @@ def scatterplot_acc_vs_gates(
                 cgates[i][j],
                 accuracies[i][j],
                 color=colors[j],
+                marker=markers[j],
                 s=40,
             )
 
     plt.title(f"{nqubits} qubits, {nlayers} layers, XXZ")
-    # plt.legend(ncols=2)
-    plt.ylabel("Absolute error")
+    plt.ylabel(r"$ 1-\frac{E}{E_0} $")
     plt.xlabel("# 2q gates")
     plt.yscale("log")
     plt.legend(loc=3)
     plt.savefig(f"{title}.png", dpi=600)
     plt.savefig(f"{title}.pdf")
-    plt.show()
-
-    plt.figure(figsize=(6, 6 * 6 / 8))
-
-    for i in range(3):
-        cg = np.array(cgates).T[i]
-        accs = np.array(accuracies).T[i]
-
-        plt.plot(epochs, accs, color=colors[i], label=f"{i+1} GCI steps")
-        for k, a in enumerate(accs):
-            plt.scatter(
-                epochs[k],
-                a,
-                color=colors[i],
-                s=20 * cg[k] / (min_cg / 2),
-            )
-    # plt.plot(
-    #     vqe_cg,
-    #     abs(losses[epochs] - true_ground_energy),
-    #     color=colors[3],
-    #     marker="o",
-    #     markersize=size,
-    #     label=f"VQE"
-    # )
-    # plt.plot(
-    #     vqe_cg_1Lmore,
-    #     abs(losses_1Lmore[epochs] - true_ground_energy),
-    #     color=colors[4],
-    #     marker="o",
-    #     markersize=size,
-    #     label=f"VQE +1L"
-    # )
-    # plt.plot(
-    #     vqe_cg_2Lmore,
-    #     abs(losses_2Lmore[epochs] - true_ground_energy),
-    #     color=colors[5],
-    #     marker="o",
-    #     markersize=size,
-    #     label=f"VQE +2L"
-    # )
-
-    plt.title(f"{nqubits} qubits, {nlayers} layers, XXZ")
-    plt.legend(ncols=2)
-    plt.ylabel("Absolute error")
-    plt.xlabel("Epochs")
-    plt.yscale("log")
-    plt.savefig(f"{title}_epochs.png")
-    plt.savefig(f"{title}_epochs.pdf")
-    plt.show()
+    # plt.show()
 
 
 path = "vqe_data/compile_targets_light/"
 true_ground_energy = -15.276131122065937
 studied_epochs = np.arange(1000, 5001, 1000)
-steps_colors = ["#f99f1e", "#f05426", "#8c1734"]
+steps_colors = ["#8c1734", "#f05426", "#f99f1e"]
 epoch_shapes = ["o", "s", "P", "d", "v"]
 dbi_steps = 3
 nq = 10
