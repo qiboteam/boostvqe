8dfc01ffb54bc0d892ca1efbc2fccce6d52757a0
Marek Gluza <marekgluza@gmail.com>
2024-07-24 09:48:34 +0800

    refactored notebook for Hamiltonian Simulation, thanks JYK for help

--- src/boostvqe/models/dbi/double_bracket_evolution_oracles.py
+++ src/boostvqe/models/dbi/double_bracket_evolution_oracles.py
@@ -1,7 +1,7 @@
 from copy import deepcopy
 from dataclasses import dataclass
 from enum import Enum, auto
-from functools import cached_property, reduce
+from functools import reduce
 from typing import Union
 
 import hyperopt
@@ -11,33 +11,27 @@ from qibo import Circuit, gates, symbols
 from qibo.config import raise_error
 from qibo.hamiltonians import AbstractHamiltonian, SymbolicHamiltonian
 
+from qibo.transpiler.unitary_decompositions import two_qubit_decomposition
+
 # TODO: remove this global import
 from boostvqe.compiling_XXZ import nqubit_XXZ_decomposition
 
 
 class EvolutionOracleType(Enum):
-    """Mode to represent evolution oracle exp{-i t H}."""
-
     numerical = auto()
-    """Matrix exponentiation."""
+    """If you will work with exp(is_k J_k) as a numerical matrix"""
 
     hamiltonian_simulation = auto()
-    """Use SymbolicHamiltonian and the compiles the circuit."""
+    """If you will use SymbolicHamiltonian"""
 
 
 @dataclass
 class EvolutionOracle:
-    """Representation of an EvolutionOracle exp{-i t h }."""
-
     h: AbstractHamiltonian
-    """Hamiltonian to be exponentiated."""
     evolution_oracle_type: EvolutionOracleType
-    """Type of evolution oracle."""
 
     def __post_init__(self):
-        if self.steps is None:
-            self.steps = 1
-        """Number of steps in Trotter-Suzuki discretization."""
+        self.steps = 1
 
     def __call__(self, t_duration: float):
         """Returns either the name or the circuit"""
@@ -48,66 +42,53 @@ class EvolutionOracle:
         In the hamiltonian_simulation mode we evaluate an appropriate Trotter-Suzuki discretization up to `self.eps_trottersuzuki` threshold.
         """
         if self.evolution_oracle_type is EvolutionOracleType.numerical:
-            return self.h.exp(t_duration)  # e^{- i t_duration H}
+            return self.h.exp(t_duration)
         else:
             dt = t_duration / self.steps
             return reduce(
                 Circuit.__add__,
-                [self.h.circuit(dt)] * self.steps,  # approx of e^{- i t_duration H}
+                [deepcopy(self.h).circuit(dt)] * self.steps,
             )
-
-    def inverted(self, duration: float):
-        if self.evolution_oracle_type is EvolutionOracleType.hamiltonian_simulation:
-            return self.circuit(duration).operator.invert()
-        elif self.evolution_oracle_type is EvolutionOracleType.numerical:
-            return np.linalg.inv(self.circuit(duration))
 
 
 @dataclass
 class FrameShiftedEvolutionOracle(EvolutionOracle):
-    """EvolutionOracle to perform FrameShift.
-
-    The new EvolutionOracle will be the following V base_evolution_oracle Vdag.
-    Where V is `before circuit` and Vdag is `after circuit.
-    """
-
-    # before_circuit: Union[Circuit, np.ndarray]
-    # after_circuit: Union[Circuit, np.ndarray]
-    circuit_frame: Union[Circuit, np.ndarray]
+    before_circuit: str
+    after_circuit: str
     base_evolution_oracle: EvolutionOracle
 
     @classmethod
     def from_evolution_oracle(
         cls,
         base_evolution_oracle: EvolutionOracle,
-        circuit_frame,
+        before_circuit,
+        after_circuit,
     ):
-        """Create instance using only new attributes of FreameShiftedEvolutionOracle."""
         return cls(
             base_evolution_oracle=base_evolution_oracle,
-            circuit_frame=circuit_frame,
+            before_circuit=before_circuit,
+            after_circuit=after_circuit,
             h=base_evolution_oracle.h,
             evolution_oracle_type=base_evolution_oracle.evolution_oracle_type,
         )
 
     @property
     def nqubits(self):
-        return self.circuit_frame.nqubits
+        assert self.before_circuit.nqubits == self.after_circuit.nqubits
+        return self.before_circuit.nqubits
 
-    def circuit(self, duration: float = None):
-        """Compute corresponding circuit."""
-
+    def circuit(self, t_duration: float = None):
         if self.evolution_oracle_type is EvolutionOracleType.numerical:
             return (
-                self.inverse_circuit
-                @ self.base_evolution_oracle(duration)
-                @ self.circuit_frame
+                self.before_circuit
+                @ self.base_evolution_oracle(t_duration)
+                @ self.after_circuit
             )
         elif self.evolution_oracle_type is EvolutionOracleType.hamiltonian_simulation:
             return (
-                self.circuit_frame
-                + self.base_evolution_oracle.circuit(duration)
-                + self.inverse_circuit
+                self.after_circuit
+                + self.base_evolution_oracle.circuit(t_duration)
+                + self.before_circuit
             )
         else:
             raise_error(
@@ -115,15 +96,7 @@ class FrameShiftedEvolutionOracle(EvolutionOracle):
                 f"You are using an EvolutionOracle type which is not yet supported.",
             )
 
-    @cached_property
-    def inverse_circuit(self):
-        if self.evolution_oracle_type is EvolutionOracleType.hamiltonian_simulation:
-            return self.circuit_frame.invert()
-        elif self.evolution_oracle_type is EvolutionOracleType.numerical:
-            return np.linalg.inv(self.circuit_frame)
-
     def get_composed_circuit(self):
-        """Collect all frame shift in circuits."""
         c = Circuit(nqubits=self.nqubits)
         fseo = self
         while isinstance(fseo, FrameShiftedEvolutionOracle):
@@ -131,20 +104,18 @@ class FrameShiftedEvolutionOracle(EvolutionOracle):
                 self.base_evolution_oracle.evolution_oracle_type
                 is EvolutionOracleType.numerical
             ):
-                c = c @ fseo.circuit_frame
+                c = c @ fseo.after_circuit
             elif (
                 self.base_evolution_oracle.evolution_oracle_type
                 is EvolutionOracleType.hamiltonian_simulation
             ):
-                c = c + fseo.circuit_frame
+                c = c + fseo.after_circuit
             fseo = fseo.base_evolution_oracle
         return c
 
 
 @dataclass
 class MagneticFieldEvolutionOracle(EvolutionOracle):
-    """Evolution oracle with MagneticField."""
-
     _params: Union[list, np.ndarray]
 
     @property
@@ -170,29 +141,6 @@ class MagneticFieldEvolutionOracle(EvolutionOracle):
         return cls(
             h=hamiltonian, evolution_oracle_type=evolution_oracle_type, _params=params
         )
-
-    def circuit(self, t):
-        """
-        Constructs an Magnetic Field model circuit for n qubits, given by:
-        .. math::
-            H(B) = \\sum_{i=0}^{N-1} B_i Z_i
-
-
-        Args:
-        nqubits (int): Number of qubits.
-        t (float): Total evolution time.
-
-        Returns:
-        Circuit: The final multi-layer circuit.
-
-        """
-        nqubits = len(self.params)
-        circuit = Circuit(nqubits=nqubits)
-        # TODO: ask Marek
-        circuit.add(
-            gates.RZ(q_i, 2 * t * b) for q_i, b in zip(range(nqubits), self.params)
-        )
-        return circuit
 
 
 @dataclass
@@ -245,7 +193,7 @@ class IsingNNEvolutionOracle(EvolutionOracle):
         Returns:
         Circuit: The final multi-layer circuit.
 
-        """
+        #"""
         nqubits = len(self.params) // 2
         circuit = Circuit(nqubits=nqubits)
         # Create lists of even and odd qubit indices
@@ -262,35 +210,6 @@ class IsingNNEvolutionOracle(EvolutionOracle):
             for q_i, b in zip(range(nqubits), self.params[:nqubits])
         )
         return circuit
-
-
-##############################################
-#pseudocode follows
-@dataclass
-class HamiltonianSimulation_EvolutionOracle(EvolutionOracle):
-    steps: int = None
-    order: int = None
-    delta: float = 0.5
-
-    @classmethod
-    def from_symbolic_hamiltonian(cls, h):
-        
-        hamiltonian = h
-        ...
-        return cls(
-            h=hamiltonian,
-            evolution_oracle_type=EvolutionOracleType.hamiltonian_simulation,
-            **kwargs,
-        )
-
-    def circuit(self, t_duration, steps=None, order=None):
-        c=self.h.circuit(dt)
-        for g in c.queue:
-            if g is two qubit
-                new_gate_list_add(two_qubit_decomposition(g)
-        c_recompiled_into_CNOT = ...collect...decompositions here
-
-        return reduce(__add__,c_recompiled_dt) #cf XXZ compiling
 
 
 @dataclass
@@ -328,7 +247,6 @@ class XXZ_EvolutionOracle(EvolutionOracle):
             steps = self.steps
         if order is None:
             order = self.order
-            # this is doing correctly e^{- i t H_XXZ}
         return nqubit_XXZ_decomposition(
             nqubits=self.h.nqubits,
             t=t_duration,
@@ -336,3 +254,38 @@ class XXZ_EvolutionOracle(EvolutionOracle):
             steps=steps,
             order=order,
         )
+
+
+@dataclass 
+class SymbolicHamiltonian_EvolutionOracle(EvolutionOracle):
+    steps: int = None 
+    order: int = None 
+    delta: float = 0.5 
+  
+    @classmethod 
+    def from_symbolic_hamiltonian(cls, symbolic_hamiltonian: SymbolicHamiltonian, **kwargs): 
+        return cls( 
+            symbolic_hamiltonian, 
+            evolution_oracle_type=EvolutionOracleType.hamiltonian_simulation, 
+            **kwargs,
+        ) 
+  
+    def circuit(self, t_duration: int, steps:int=1, order=None): 
+        dt = t_duration/steps    
+        c = self.h.circuit(dt) 
+        nqubits = c.nqubits
+        c_recompiled_into_CNOT = Circuit(nqubits=nqubits)
+        
+        for gate in c.queue: 
+            if len(gate.qubits) > 1: #if gate is two qubit 
+                gate_decomposition = two_qubit_decomposition(*gate.qubits, gate.matrix())
+                for gate_elem in gate_decomposition:
+                    c_recompiled_into_CNOT.add(gate_elem)
+            else:
+                c_recompiled_into_CNOT.add(gate)
+
+        multi_layer = Circuit(nqubits=nqubits)
+        for _ in range(steps):
+            multi_layer += c_recompiled_into_CNOT 
+  
+        return multi_layer 
